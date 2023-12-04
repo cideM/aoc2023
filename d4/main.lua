a1d03f192973deaec263f4608f33f488434377d1
Florian Beeres <yuuki@protonmail.com>
2023-12-04 11:48:05 +0100

    Add comments

--- d4/main.lua
+++ d4/main.lua
@@ -1,9 +1,13 @@
 -- completed in 55m30s74
+
 local P1, P2, CARDS, CACHE = 0, 0, {}, {}
 for line in io.lines() do
 	local id, delim_start, win_ids = tonumber(line:match("(%d+)")), line:find("|"), {}
 	assert(id)
 
+	-- Go through the numbers after the | and check if they can be found
+	-- in the part of the line before |. For each match, add another
+	-- card ID to the list of cards spawned by the current card.
 	for d in line:gmatch("%d+", delim_start) do
 		if line:sub(1, delim_start):match(" " .. d .. " ") then
 			table.insert(win_ids, id + #win_ids + 1)
@@ -27,6 +31,11 @@ local function resolve(id)
 	return CACHE[id]
 end
 
+-- Go through the cards backwards, since cards can only spawn cards below
+-- themselves. Meaning, the last card can't spawn anything (indeed it never
+-- has any winners). For each card spawned by a card, recursively evaluate
+-- how many cards would be spawned in turn. Remember the number of cards
+-- spawned by each card in CACHE, so we don't need to do work twice.
 P2 = #CARDS
 for id = #CARDS, 1, -1 do
 	P2 = P2 + resolve(id)
