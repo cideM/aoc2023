165b5df6ccf0eea7909cd4308afa96eae188b53c
Florian Beeres <yuuki@protonmail.com>
2023-12-04 12:18:38 +0100

    Refactor

--- d4/main.lua
+++ d4/main.lua
@@ -1,44 +1,19 @@
 -- completed in 55m30s74
 
-local P1, P2, CARDS, CACHE = 0, 0, {}, {}
+local P1, P2, CARDS = 0, 0, {}
 for line in io.lines() do
-	local id, delim_start, win_ids = tonumber(line:match("(%d+)")), line:find("|"), {}
-	assert(id)
+	local id, delim_start, matches = tonumber(line:match("(%d+)")), line:find("|"), 0
+	CARDS[id] = (CARDS[id] or 0) + 1
 
 	-- Go through the numbers after the | and check if they can be found
 	-- in the part of the line before |. For each match, add another
 	-- card ID to the list of cards spawned by the current card.
 	for d in line:gmatch("%d+", delim_start) do
 		if line:sub(1, delim_start):match(" " .. d .. " ") then
-			table.insert(win_ids, id + #win_ids + 1)
+			CARDS[id + 1 + matches] = (CARDS[id + 1 + matches] or 0) + CARDS[id]
+			matches = matches + 1
 		end
 	end
-	CARDS[id] = win_ids
-	P1 = P1 + (#win_ids == 0 and 0 or 2 ^ (#win_ids - 1))
+	P1, P2 = P1 + (matches == 0 and 0 or 2 ^ (matches - 1)), P2 + CARDS[id]
 end
-
-local function resolve(id)
-	if CACHE[id] then
-		return CACHE[id]
-	end
-
-	local added = #CARDS[id]
-	for _, other in ipairs(CARDS[id]) do
-		added = added + resolve(other)
-	end
-
-	CACHE[id] = added
-	return CACHE[id]
-end
-
--- Go through the cards backwards, since cards can only spawn cards below
--- themselves. Meaning, the last card can't spawn anything (indeed it never
--- has any winners). For each card spawned by a card, recursively evaluate
--- how many cards would be spawned in turn. Remember the number of cards
--- spawned by each card in CACHE, so we don't need to do work twice.
-P2 = #CARDS
-for id = #CARDS, 1, -1 do
-	P2 = P2 + resolve(id)
-end
-
 print(P1, P2)
