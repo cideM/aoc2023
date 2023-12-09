6469585bd7f54084d23616f18abae3799257bed0
Florian Beeres <yuuki@protonmail.com>
2023-12-09 11:35:42 +0100

    refactor

--- d9/main.lua
+++ d9/main.lua
@@ -1,14 +1,11 @@
 -- Solved in 24m58s19
 local function extrapolate(seq)
-	local every_is_zero = table.concat(seq):match("^0+$")
-
-	if every_is_zero then
+	if table.concat(seq):match("^0+$") then
 		return 0, 0
 	else
 		local diffs = {}
 		for i = 2, #seq, 1 do
-			local a, b = seq[i - 1], seq[i]
-			table.insert(diffs, b - a)
+			table.insert(diffs, seq[i] - seq[i - 1])
 		end
 		local front, back = extrapolate(diffs)
 		return seq[1] - front, seq[#seq] + back
@@ -22,7 +19,6 @@ for line in io.lines() do
 		table.insert(seq, tonumber(d))
 	end
 	local front, back = extrapolate(seq)
-	P1 = P1 + back
-	P2 = P2 + front
+	P1, P2 = P1 + back, P2 + front
 end
 print(P1, P2)
