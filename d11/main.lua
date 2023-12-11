b6d7065edaad38a9c678d6ecc29c145e45e07bf4
Florian Beeres <yuuki@protonmail.com>
2023-12-11 11:50:15 +0100

    Refactor d11

--- d11/main.lua
+++ d11/main.lua
@@ -1,10 +1,13 @@
-local GRID = {}
+local GRID, GALAXIES = {}, {}
 do
 	-- Parse the input and insert markers for expanded rows
 	for line in io.lines() do
 		local row = {}
 		for c in line:gmatch(".") do
 			table.insert(row, c)
+			if c == "#" then
+				table.insert(GALAXIES, { #row, #GRID + 1 })
+			end
 		end
 		table.insert(GRID, row)
 		if line:match("^%.+$") then
@@ -28,54 +31,19 @@ do
 	end
 end
 
--- bfs calculates the distances between "start" and *all* other points
--- in the grid. It's inefficient but you can get away with it here. It
--- doesn't store the distance as a number but as a list of steps. That way
--- it's easy to know how many markers were traveresed, so we can then
--- arbitrarily adjust the actual distance by giving the markers different
--- distances.
-local function bfs(start) -- {x,y}
-	local queue, distances = { start }, { [start[1] .. ";" .. start[2]] = {} }
-	while #queue > 0 do
-		local current = table.remove(queue, 1)
-		local x, y = table.unpack(current)
-		for _, vector in ipairs({ { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } }) do
-			local dx, dy = table.unpack(vector)
-			local x2, y2 = x + dx, y + dy
-			local neighbour = (GRID[y2] or {})[x2]
-			if neighbour and not distances[x2 .. ";" .. y2] then
-				table.insert(queue, { x2, y2 })
-				local path_until_here = distances[x .. ";" .. y]
-				local copy = table.move(path_until_here, 1, #path_until_here, 1, {})
-				table.insert(copy, neighbour)
-				distances[x2 .. ";" .. y2] = copy
-			end
-		end
-	end
-	return distances
-end
-
--- Find the locations of all galaxies
-local galaxies = {}
-for y, row in ipairs(GRID) do
-	for x, col in ipairs(row) do
-		if col == "#" then
-			table.insert(galaxies, { x, y })
-		end
-	end
-end
-
--- For each galaxy, calculate the paths to all other points.
--- Then, find just the paths to other galaxies and walk them again,
--- but this time counting "." as 1 and "x" (expansion marker) as either
--- 2 (part 1) or 1000000 (part 2).
+-- For each pair walk left -> right and top -> bottom and
+-- add 1 for each normal step and 2 or 1000000 for each
+-- expanded row/column
 local p1, p2 = 0, 0
-for i, galaxy in ipairs(galaxies) do
-	local current_distances = bfs(galaxy)
-	for _, other in ipairs({ table.unpack(galaxies, i + 1) }) do
-		for _, step in ipairs(current_distances[table.concat(other, ";")]) do
-			p1 = p1 + (step == "x" and 2 or 1)
-			p2 = p2 + (step == "x" and 1000000 or 1)
+for i, galaxy in ipairs(GALAXIES) do
+	for _, other in ipairs({ table.unpack(GALAXIES, i + 1) }) do
+		for x = math.min(galaxy[1], other[1]) + 1, math.max(galaxy[1], other[1]) do
+			p1 = p1 + (GRID[1][x] == "x" and 2 or 1)
+			p2 = p2 + (GRID[1][x] == "x" and 1000000 or 1)
+		end
+		for y = math.min(galaxy[2], other[2]) + 1, math.max(galaxy[2], other[2]) do
+			p1 = p1 + (GRID[y][1] == "x" and 2 or 1)
+			p2 = p2 + (GRID[y][1] == "x" and 1000000 or 1)
 		end
 	end
 end
