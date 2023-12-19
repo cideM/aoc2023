7bf4d7c17a0509f41ff4932ac46b74a6f44b7e4b
Florian Beeres <yuuki@protonmail.com>
2023-12-19 12:29:40 +0100

    Refactor and speed up d18 scanlines by using a heap

--- d18/scanlines.lua
+++ d18/scanlines.lua
@@ -1,3 +1,76 @@
+local Heap = {}
+
+function Heap:new()
+	local o = {}
+	self.__index = self
+	setmetatable(o, self)
+	return o
+end
+
+function Heap:bubbleup(pos)
+	while pos > 1 do
+		local parent = math.floor(pos / 2)
+		if not (self[pos].score < self[parent].score) then
+			break
+		end
+		self[parent], self[pos] = self[pos], self[parent]
+		pos = parent
+	end
+end
+
+function Heap:sink(pos)
+	local last = #self
+	while true do
+		local min, child = pos, pos * 2
+		for c = child, child + 1 do
+			if c <= last and self[c].score < self[min].score then
+				min = c
+			end
+		end
+
+		if min == pos then
+			break
+		end
+		self[pos], self[min] = self[min], self[pos]
+		pos = min
+	end
+end
+
+function Heap:insert(value)
+	local pos = #self + 1
+	self[pos] = value
+	self:bubbleup(pos)
+end
+
+function Heap:remove(pos)
+	local last = #self
+	if pos == last then
+		local v = self[last]
+		self[last] = nil
+		return v
+	end
+
+	local v = self[pos]
+	self[pos], self[last] = self[last], self[pos]
+	self[last] = nil
+	self:bubbleup(pos)
+	self:sink(pos)
+	return v
+end
+
+local CHAR_TO_DIR = { L = { -1, 0 }, U = { 0, -1 }, R = { 1, 0 }, D = { 0, 1 } }
+local CHAR_TO_SYMBOL = { L = "<", U = "^", R = ">", D = "v" }
+local CORNERS = {
+	[">^"] = "J",
+	["^>"] = "F",
+	[">v"] = "7",
+	["v>"] = "L",
+	["v<"] = "J",
+	["<v"] = "F",
+	["<^"] = "L",
+	["^<"] = "7",
+}
+
 local input = {}
 for line in io.lines() do
 	table.insert(input, line)
@@ -10,92 +83,83 @@ local function make_grid(lines, parse_line)
 			return dir, steps
 		end
 
-	local perimeter = 0
-	local rows = {}
+	local perimeter, rows, loop = 0, {}, {}
 
-	print "grid"
 	do
-		local directions =
-			{ L = { -1, 0 }, U = { 0, -1 }, R = { 1, 0 }, D = { 0, 1 } }
-		local symbols = { L = "-", U = "|", R = "-", D = "|" }
-		local corner_symbols = {
-			RU = "J",
-			UR = "F",
-			RD = "7",
-			DR = "L",
-			DL = "J",
-			LD = "F",
-			LU = "L",
-			UL = "7",
-		}
-
 		local x, y = 1, 1
-		local previous_dir_letter, previous_key = nil, nil
-		local first_dir_letter_after_start = nil
-		for i, line in ipairs(lines) do
-			print(i .. "/" .. #lines)
-			local current_dir_letter, steps = parse_line(line)
+		for _, line in ipairs(lines) do
+			local char, steps = parse_line(line)
 			steps = tonumber(steps)
+			local dir = CHAR_TO_DIR[char]
+
 			perimeter = perimeter + steps
-			local direction = directions[current_dir_letter]
-			for _ = 1, steps do
-				if
-					previous_dir_letter
-					and previous_key
-					and previous_dir_letter ~= current_dir_letter
-				then
-					rows[previous_key[2]][previous_key[1]] =
-						corner_symbols[previous_dir_letter .. current_dir_letter]
-				end
 
-				x, y = x + direction[1], y + direction[2]
-				if not rows[y] then
-					rows[y] = {}
+			-- We compress horizontal steps. If steps = 1000 then we only insert two steps,
+			-- where the first covers 999 distance and the second 1. That way we still
+			-- get proper corners for replacing symbols later. We don't compress vertical
+			-- steps so everything ends up in the correct row.
+			local compressed = {}
+			if (char == "L" or char == "R") and steps > 1 then
+				compressed = { steps - 1, 1 }
+			else
+				for _ = 1, steps do
+					table.insert(compressed, 1)
 				end
-				rows[y][x] = symbols[current_dir_letter]
+			end
 
-				if not first_dir_letter_after_start then
-					first_dir_letter_after_start = current_dir_letter
+			for _, step in ipairs(compressed) do
+				local x2, y2 = x + dir[1] * step, y + dir[2] * step
+				if not rows[y2] then
+					rows[y2] = Heap:new()
 				end
-
-				-- We've reached the first node again and need to link it to
-				-- the next.
-				if x == 1 and y == 1 and first_dir_letter_after_start then
-					rows[y][x] =
-						corner_symbols[current_dir_letter .. first_dir_letter_after_start]
-				end
-
-				previous_dir_letter, previous_key = current_dir_letter, { x, y }
+				local point = { { x + dir[1], x2 }, CHAR_TO_SYMBOL[char] }
+				rows[y2]:insert { score = point[1][1], point = point }
+				table.insert(loop, point)
+				x, y = x2, y2
 			end
 		end
 	end
 
-	print "sort rows by x"
-	for y, r in pairs(rows) do
-		local xs = {}
-		for x in pairs(r) do
-			table.insert(xs, x)
+	-- Go through the loop once more and replace corners with
+	-- connector symbols, which are used for the scanlines function
+	-- later.
+	do
+		for i, p in ipairs(loop) do
+			local prev = loop[(((i - 1) - 1) % #loop) + 1]
+			local corner = prev[2] .. p[2]
+			if CORNERS[corner] then
+				prev[2] = CORNERS[corner]
+			end
 		end
-		table.sort(xs, function(a, b)
-			return a < b
-		end)
-		local r2 = {}
-		for _, x in ipairs(xs) do
-			table.insert(r2, { x, r[x] })
-		end
-		rows[y] = r2
 	end
 
-	print "insert ."
-	for y, r in pairs(rows) do
-		local r2 = { table.remove(r, 1) }
-		for _, v in ipairs(r) do
-			local last_x, x = r2[#r2][1], v[1]
-			if x - last_x == 1 then
-				table.insert(r2, v)
+	-- We no longer need the heap, only the raw data in its
+	-- sorted form. So get the values out of each heap (~ row)
+	for y, row in pairs(rows) do
+		local points = {}
+		while #row > 0 do
+			table.insert(points, row:remove(1).point)
+		end
+		rows[y] = points
+	end
+
+	-- Insert "." tiles between non-adjacent cells. Each such
+	-- tile will have the distance it covers as a value.
+	for y, row in pairs(rows) do
+		local r2 = { table.remove(row, 1) }
+		for _, point in ipairs(row) do
+			local x_range = point[1]
+			local start = math.min(x_range[1], x_range[2])
+
+			local previous_x_range = r2[#r2][1]
+			local previous_end =
+				math.max(previous_x_range[1], previous_x_range[2])
+
+			if start - previous_end == 1 then
+				table.insert(r2, point)
 			else
-				table.insert(r2, { last_x + 1, ".", x - (last_x + 1) })
-				table.insert(r2, v)
+				table.insert(r2, { previous_end + 1, ".", start - 1 })
+				table.insert(r2, point)
 			end
 		end
 		rows[y] = r2
@@ -104,31 +168,37 @@ local function make_grid(lines, parse_line)
 	return { rows, perimeter }
 end
 
+local CONNECTOR_TO_INT = { L = 1, J = 1, ["7"] = -1, F = -1 }
 local function scanline(row)
 	local inside, last_wall, score = false, nil, 0
-	-- up: 1; down: -1
-	local up_or_down = { L = 1, J = 1, ["7"] = -1, F = -1 }
+
 	for _, cell in ipairs(row) do
-		local tile = cell[2]
-		-- print(x, tile, inside)
-		if tile == "-" then
+		local tile, is_dot = cell[2], cell[2] == "."
+		if tile == ">" or tile == "<" or (is_dot and not inside) then
 			goto continue
-		elseif tile == "." and not inside then
-			last_wall = nil
-			goto continue
-		elseif tile == "|" then
+		elseif tile == "v" or tile == "^" then
 			inside = not inside
-		elseif tile == "." and inside then
-			score = score + cell[3]
-			last_wall = nil
-		elseif last_wall and up_or_down[tile] + up_or_down[last_wall] == 0 then
+		elseif is_dot and inside then
+			score = score + (cell[3] - cell[1] + 1)
+		elseif
+			last_wall
+			and CONNECTOR_TO_INT[tile] + CONNECTOR_TO_INT[last_wall] == 0
+		then
+			-- The reason for converting connectors to int is that we only flip
+			-- inside/outside when we encounter something like:
+			-- L___________
+			--             7
+			-- I'm using underscore to make it clearer that the L and 7 point in
+			-- opposite directions. Hence we give up symbols 1 and down symbols -1
+			-- so they cancel each other out.
 			inside = not inside
-			last_wall = tile
 		elseif tile == "F" or tile == "L" or tile == "J" or tile == "7" then
 			last_wall = tile
 		end
+
 		::continue::
 	end
+
 	return score
 end
 
@@ -138,19 +208,10 @@ local function parse_lines_p2(s)
 	return dirs[direction], tonumber("0x" .. distance)
 end
 
-for _, data in ipairs {
-	make_grid(input),
-	make_grid(input, parse_lines_p2),
-} do
+for _, data in ipairs { make_grid(input), make_grid(input, parse_lines_p2) } do
 	local grid, area = data[1], data[2]
-
-	print "scan"
 	for _, row in pairs(grid) do
-		local enclosed = scanline(row)
-		area = area + enclosed
+		area = area + scanline(row)
 	end
-
 	print(area)
 end
-
--- printgrid(GRID, MIN_X, MAX_X, MIN_Y, MAX_Y)
